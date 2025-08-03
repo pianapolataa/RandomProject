8457938bcf470a839a3a4206569c5617800cf56e
Haokai Ma <mahaokai9@gmail.com>
2025-08-03 14:29:15 -0400

    Steps/limit while training

--- path_env.py
+++ path_env.py
@@ -147,13 +147,16 @@ class CarPathEnv(gym.Env):
         self.prev_closest_wp = closest_wp
 
         terminated = bool(self.dist_to_path > 20.0)
-        steps = 220
+        steps = 300
         if self.path_type == "alternating":
             steps = 1000
         elif self.path_type == "straight":
-            steps = 150
+            steps = 200
         # print(self.path)
-        truncated = bool(self.step_count >= steps)
+        lim = 50
+        if self.path_type == "alternating":
+            lim = 200
+        truncated = bool(self.step_count >= steps or self.x >= lim)
 
         info = {}
 
